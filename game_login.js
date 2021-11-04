6defbf167cc001f9b94bbf3439311da59d5a3ddc
JayAuradkar <80576839+JayAuradkar@users.noreply.github.com>
2021-11-04 13:31:45 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+var player1 = "";
+var player2 = "";
+function Login(){
+    player1 = document.getElementById("P1L").value
+    player2 = document.getElementById("P2L").value
+    localStorage.setItem("storage_1",player1)
+    localStorage.setItem("storage_2",player2)
+    window.location=""
+}
